6f5f924625a04e16397e9b311571fe82538bee1b
jkidwell <jay@contensive.com>
2024-06-12 00:50:07 -0400

    convert to vs project

---
+++ source/CodeMirrorEditor/Controllers/GenericController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contensive.CodeMirrorEditor.Controllers {
+    public class GenericController {
+        //
+        //=================================================================================
+        /// <summary>
+        /// Get a Random integer Value
+        /// </summary>
+        /// <param name="core"></param>
+        /// <returns></returns>
+        public static int getRandomInteger() {
+            byte[] rngBytes = new byte[4];
+            RandomNumberGenerator.Create().GetBytes(rngBytes);
+            int myInt = BitConverter.ToInt32(rngBytes, 0);
+            if (myInt > 0) { return myInt; }
+            return 0 - myInt;
+        }
+        //
+        //=================================================================================
+        /// <summary>
+        /// Get a Random integer Value between 0 and maxValue
+        /// </summary>
+        /// <param name="core"></param>
+        /// <param name="maxValue"></param>
+        /// <returns></returns>
+        public static int getRandomInteger(int maxValue) {
+            return Convert.ToInt32(maxValue * (double)getRandomInteger() / (double)Int32.MaxValue);
+
+        }
+        //
+        //=================================================================================
+        /// <summary>
+        /// generate a random alphanumeric string, upper and lower case text and numerics (0...9, a...z, A...Z, no special characters)
+        /// </summary>
+        /// <param name="core"></param>
+        /// <param name="length"></param>
+        /// <returns></returns>
+        public static string getRandomString(int length) {
+            var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            var stringChars = new char[length];
+            for (int i = 0; i < length; i++) {
+                stringChars[i] = chars[getRandomInteger(chars.Length - 1)];
+            }
+            return new String(stringChars);
+        }
+    }
+}
---
+++ source/CodeMirrorEditor/Views/CodeMirrorHtmlEditorRemote.cs
@@ -0,0 +1,42 @@
+﻿using Contensive.BaseClasses;
+using Contensive.CodeMirrorEditor.Controllers;
+using System;
+
+namespace Contensive.CodeMirrorEditor.Views {
+    public class CodeMirrorHtmlEditorRemote : AddonBaseClass {
+        public override object Execute(CPBaseClass cp) {
+            try {
+                string editorName = cp.Doc.GetText("editorName");
+                string editorValue = cp.Utils.EncodeHTML(cp.Doc.GetText("editorValue"));
+                bool readOnly = cp.Doc.GetBoolean("editorReadOnly");
+                string editorId = cp.Doc.GetText("editorId");
+                if (string.IsNullOrEmpty(editorId)) { editorId = GenericController.getRandomString(8); }
+                cp.Doc.AddHeadStyleLink("/codemirror-5.52.0/lib/codemirror.css");
+                cp.Doc.AddHeadStyleLink("/codemirror-5.52.0/addon/display/fullscreen.css");
+                cp.Doc.AddHeadStyleLink("/codemirror-5.52.0/theme/idea.css");
+                cp.Doc.AddHeadJavascriptLink("/codemirror-5.52.0/lib/codemirror.js");
+                cp.Doc.AddHeadJavascriptLink("/codemirror-5.52.0/addon/display/fullscreen.js");
+                cp.Doc.AddHeadJavascriptLink("/codemirror-5.52.0/addon/display/autorefresh.js");
+                //
+                cp.Doc.AddHeadJavascriptLink("/codemirror-5.52.0/mode/xml/xml.js");
+                cp.Doc.AddHeadJavascriptLink("/codemirror-5.52.0/mode/javascript/javascript.js");
+                cp.Doc.AddHeadJavascriptLink("/codemirror-5.52.0/mode/css/css.js");
+                cp.Doc.AddHeadJavascriptLink("/codemirror-5.52.0/mode/htmlmixed/htmlmixed.js");
+                //
+                string result = "" +
+                    $"<div class=codeMirrorEditor>" +
+                    $"<textarea id=\"{editorId}\" name=\"{editorName}\" rows=\"30\">{editorValue}</textarea>" +
+                    $"</div>" +
+                    $"<script type=\"text/javascript\">" +
+                    $"document.addEventListener(\"DOMContentLoaded\", function(event){{" +
+                        $"initCodeMirrorHtmlEditor(\"{editorId}\",{(readOnly ? "true" : "false")});" +
+                    $"}});" +
+                    $"</script>";
+                return result;
+            } catch (Exception ex) {
+                cp.Site.ErrorReport(ex);
+                throw;
+            }
+        }
+    }
+}
